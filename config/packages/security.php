10fc80eeb352ee7b8ca58fa4cbaae13cf687c7da
jfsenechal <jfsenechal@gmail.com>
2021-06-03 15:50:26 +0200

    new config builder

---
+++ config/packages/security.php
@@ -0,0 +1,11 @@
+<?php
+use Symfony\Config\SecurityConfig;
+
+return static function (SecurityConfig $security) {
+    $security->firewall('main')
+        ->provider('bottin_sql_provider')
+        ->lazy(true);
+
+    $security
+        ->accessControl(['path' => '^/admin', 'roles' => 'ROLE_ADMIN']);
+};
