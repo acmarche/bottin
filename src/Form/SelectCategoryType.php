2ead6cd8cd2ab0f2fe7c60cf16ba2851ed6aedff
jfsenechal <jfsenechal@gmail.com>
2021-08-10 21:35:13 +0200

    fix classement

--- src/Form/SelectCategoryType.php
+++ src/Form/SelectCategoryType.php
@@ -8,10 +8,6 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class SelectCategoryType extends AbstractType
 {
-    /**
-     * @param FormBuilderInterface $formBuilder
-     * @param array $options
-     */
     public function buildForm(FormBuilderInterface $formBuilder, array $options): void
     {
         $formBuilder
@@ -20,7 +16,7 @@ class SelectCategoryType extends AbstractType
                 HiddenType::class,
                 [
                     'required' => true,
-                    'attr' => ['data-category-target' => 'select'],
+                    'attr' => ['data-classement-target' => 'selectedCategory'],
                 ]
             );
     }
