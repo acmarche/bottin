3eb7232736a3961910b1df9a048a29f3db627299
jfsenechal <jfsenechal@gmail.com>
2021-09-01 09:30:23 +0200

    fix html

--- config/packages/twig.php
+++ config/packages/twig.php
@@ -5,5 +5,6 @@ use Symfony\Config\TwigConfig;
 return static function (TwigConfig $twig) {
     $twig
         ->formThemes(['bootstrap_5_layout.html.twig'])
-        ->path('%kernel.project_dir%/src/AcMarche/Bottin/templates', 'AcMarcheBottin');
+        ->path('%kernel.project_dir%/src/AcMarche/Bottin/templates', 'AcMarcheBottin')
+        ->global('bootcdn')->value('https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css');
 };
