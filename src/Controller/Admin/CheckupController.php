c8106f987b6f1ee8b5ec784cfa14e334c2a42935
jfsenechal <jfsenechal@gmail.com>
2021-09-13 15:08:39 +0200

    principal

--- src/Controller/Admin/CheckupController.php
+++ src/Controller/Admin/CheckupController.php
@@ -3,6 +3,8 @@
 namespace AcMarche\Bottin\Controller\Admin;
 
 use AcMarche\Bottin\Category\Repository\CategoryService;
+use AcMarche\Bottin\Repository\ClassementRepository;
+use AcMarche\Bottin\Repository\FicheRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,11 +13,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class CheckupController extends AbstractController
 {
     private CategoryService $categoryService;
+    private FicheRepository $ficheRepository;
+    private ClassementRepository $classementRepository;
 
     public function __construct(
-        CategoryService $categoryService
+        CategoryService $categoryService,
+        FicheRepository $ficheRepository,
+        ClassementRepository $classementRepository
     ) {
         $this->categoryService = $categoryService;
+        $this->ficheRepository = $ficheRepository;
+        $this->classementRepository = $classementRepository;
     }
 
     /**
@@ -27,9 +35,45 @@ class CheckupController extends AbstractController
         $categories = $this->categoryService->getEmpyCategories();
 
         return $this->render(
-            '@AcMarcheBottin/admin/default/empty.html.twig',
+            '@AcMarcheBottin/admin/checkup/empty.html.twig',
             [
                 'categories' => $categories,
+            ]
+        );
+    }
+
+    /**
+     * @Route("/admin/secteur/principal", name="bottin_admin_secteur_principal")
+     * @IsGranted("ROLE_BOTTIN_ADMIN")
+     */
+    public function principal(): Response
+    {
+        $fiches = $this->ficheRepository->findAllWithJoins();
+        $data = [];
+
+        foreach ($fiches as $fiche) {
+            $classements = $fiche->getClassements();
+            if (1 == $classements->count()) {
+                $classements[0]->setPrincipal(true);
+            }
+            $principaux = array_filter($classements->toArray(), function ($classement) {
+                if ($classement->getPrincipal()) {
+                    return true;
+                }
+
+                return false;
+            });
+            if (0 == count($principaux)) {
+                $data[] = $fiche;
+            }
+        }
+
+        $this->classementRepository->flush();
+
+        return $this->render(
+            '@AcMarcheBottin/admin/checkup/principal.html.twig',
+            [
+                'fiches' => $data,
             ]
         );
     }
