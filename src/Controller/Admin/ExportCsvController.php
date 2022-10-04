af7c68ae76f4587343367b28aa54ed8cfd0b0327
jfsenechal <jfsenechal@gmail.com>
2022-10-04 15:38:33 +0200

    fix export

--- src/Controller/Admin/ExportCsvController.php
+++ src/Controller/Admin/ExportCsvController.php
@@ -11,9 +11,6 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * Export controller.
- */
 #[Route(path: '/admin/export/csv')]
 #[IsGranted(data: 'ROLE_BOTTIN_ADMIN')]
 class ExportCsvController extends AbstractController
@@ -41,13 +38,11 @@ class ExportCsvController extends AbstractController
     public function fichesXls(): BinaryFileResponse
     {
         $spreadsheet = $this->csvGenerator->ficheXSLObject();
-        $spreadsheet->getProperties()->setCreator('intranet')
-            ->setTitle('Liste des contacts');
         $xlsx = new Xlsx($spreadsheet);
         $temp_file = tempnam(sys_get_temp_dir(), 'fiches.xls');
         // Create the excel file in the tmp directory of the system
         $xlsx->save($temp_file);
         // Return the excel file as an attachment
-        return $this->file($temp_file, 'fiches.xls', ResponseHeaderBag::DISPOSITION_INLINE);
+        return $this->file($temp_file, 'fiches.xls', ResponseHeaderBag::DISPOSITION_ATTACHMENT);
     }
 }
