2b80214fd1c1a9b4303649cd9742d5f0d1be7e34
jfsenechal <jfsenechal@gmail.com>
2023-06-01 10:25:19 +0200

    add login_throttling

--- config/packages/security.php
+++ config/packages/security.php
@@ -35,6 +35,9 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         'logout' => ['path' => 'app_logout'],
         'form_login' => [],
         'entry_point' => BottinAuthenticator::class,
+        'login_throttling' => [
+            'max_attempts' => 6, //per minute...
+        ],
     ];
 
     if (interface_exists(LdapInterface::class)) {
