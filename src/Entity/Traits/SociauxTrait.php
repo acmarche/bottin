0625b371395e5af50d6162982e49fc0b4ddf15b0
jfsenechal <jfsenechal@gmail.com>
2024-01-30 11:04:25 +0100

    add tiktok

--- src/Entity/Traits/SociauxTrait.php
+++ src/Entity/Traits/SociauxTrait.php
@@ -6,12 +6,15 @@ use Doctrine\ORM\Mapping as ORM;
 
 trait SociauxTrait
 {
-    #[ORM\Column(type: 'string', nullable: true)]
+    #[ORM\Column(nullable: true)]
     public ?string $facebook = null;
 
-    #[ORM\Column(type: 'string', nullable: true)]
+    #[ORM\Column(nullable: true)]
     public ?string $twitter = null;
 
-    #[ORM\Column(type: 'string', nullable: true)]
+    #[ORM\Column(nullable: true)]
     public ?string $instagram = null;
+
+    #[ORM\Column(nullable: true)]
+    public ?string $tiktok = null;
 }
