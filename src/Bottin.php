3cdeb43e1b058f27704a175cc67e04c3d1e9a999
jfsenechal <jfsenechal@gmail.com>
2024-02-01 11:21:18 +0100

    add skip

--- src/Bottin.php
+++ src/Bottin.php
@@ -13,6 +13,8 @@ class Bottin
     public const ROLES = ['ROLE_BOTTIN_ADMIN', 'ROLE_BOTTIN'];
     public const url = 'https://www.marche.be/logo/adl/categories/';
 
+   public const SEARCH_SKIP = [705];
+
     public const ROOTS = [
         self::ADMINISTRATION,
         self::CITOYEN,
