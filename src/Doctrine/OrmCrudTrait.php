ceb67b9afe4ec6580a666a3b6c5766aa9fd7b2a1
jfsenechal <jfsenechal@gmail.com>
2024-02-06 08:32:17 +0100

    replace em by ->getEntityManager()

--- src/Doctrine/OrmCrudTrait.php
+++ src/Doctrine/OrmCrudTrait.php
@@ -2,13 +2,8 @@
 
 namespace AcMarche\Bottin\Doctrine;
 
-use Doctrine\ORM\EntityManager;
-
 trait OrmCrudTrait
 {
-    /** @var EntityManager */
-    protected $_em;
-
     public function insert(object $object): void
     {
         $this->persist($object);
@@ -17,21 +12,21 @@ trait OrmCrudTrait
 
     public function persist(object $object): void
     {
-        $this->_em->persist($object);
+        $this->getEntityManager()->persist($object);
     }
 
     public function flush(): void
     {
-        $this->_em->flush();
+        $this->getEntityManager()->flush();
     }
 
     public function remove(object $object): void
     {
-        $this->_em->remove($object);
+        $this->getEntityManager()->remove($object);
     }
 
     public function getOriginalEntityData(object $object): array
     {
-        return $this->_em->getUnitOfWork()->getOriginalEntityData($object);
+        return $this->getEntityManager()->getUnitOfWork()->getOriginalEntityData($object);
     }
 }
