ebd894d44f96f7eeba3c50d5934726a780ad1fd4
jfsenechal <jfsenechal@gmail.com>
2024-02-26 11:10:34 +0100

    down level error

--- src/Search/SearchMeili.php
+++ src/Search/SearchMeili.php
@@ -115,7 +115,7 @@ class SearchMeili implements SearchEngineInterface
             }
         }
 
-        $this->logger->error('MEILI: '.join(',', $filter).' END');
+        $this->logger->info('MEILI: '.join(',', $filter).' END');
 
         return $index->search('', [
             'filter' => $filter,
