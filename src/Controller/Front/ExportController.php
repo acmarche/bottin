9a2753733fcfd05992f4d2d0b512680fbd5ea543
jfsenechal <jfsenechal@gmail.com>
2024-03-25 11:35:37 +0100

    try Gotenberg

--- src/Controller/Front/ExportController.php
+++ src/Controller/Front/ExportController.php
@@ -7,8 +7,16 @@ use AcMarche\Bottin\Search\SearchEngineInterface;
 use AcMarche\Bottin\Tag\Repository\TagRepository;
 use AcMarche\Bottin\Utils\PdfDownloaderTrait;
 use AcMarche\Bottin\Utils\SortUtils;
+use Gotenberg\Exceptions\GotenbergApiErrored;
+use Gotenberg\Exceptions\NoOutputFileInResponse;
+use Gotenberg\Gotenberg;
+use Gotenberg\Stream;
+use Psr\Http\Client\ClientExceptionInterface;
+use Psr\Http\Client\ClientInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -20,7 +28,8 @@ class ExportController extends AbstractController
         #[Autowire('%kernel.project_dir%')]
         private string $project_dir,
         private readonly TagRepository $tagRepository,
-        private readonly SearchEngineInterface $meilisearch
+        private readonly SearchEngineInterface $meilisearch,
+        private ClientInterface $httpClient,
     ) {
     }
 
@@ -54,6 +63,40 @@ class ExportController extends AbstractController
             '@AcMarcheBottin/front/circuit-court/index.html.twig',
             ['hits' => $hits, 'css' => $css]
         );
+
+        $apiUrl = 'http://localhost:3000';
+        $urlPdf = 'https://bottin.marche.be/export/circuit-court';
+        $filesystem = new Filesystem();
+        $filePath = $this->project_dir.'/var/cache/index.html';
+        $filesystem->dumpFile($filePath, $html);
+
+        $chromium = Gotenberg::chromium($apiUrl);
+        $request = $chromium->html(Stream::string('index.html', $html));
+        $response = $this->httpClient->sendRequest($request);
+        $stream = $response->getBody();
+
+        $request = Gotenberg::chromium('http://localhost:3001')
+            ->pdf()
+            ->url('https://bottin.marche.be/export/circuit-court');
+
+        try {
+            $response = $this->httpClient->sendRequest($request);
+            dump($response);
+        } catch (ClientExceptionInterface $e) {
+            dump($e->getMessage());
+        }
+
+        $stream = Stream::string('/tmp/t.txt', $html);
+        try {
+            $filename = Gotenberg::save(
+                Gotenberg::chromium($apiUrl)->pdf()
+                    ->html($stream),
+                '/tmp'
+            );
+        } catch (GotenbergApiErrored|NoOutputFileInResponse $e) {
+            dump($e->getMessage());
+        }
+        dd($filename);
 
         return new Response($html);
 
