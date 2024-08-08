3a8a8a3c22d810cec56c66a2bf543079342c79a8
jfsenechal <jfsenechal@gmail.com>
2024-08-08 12:29:54 +0200

    try search

--- src/Command/MeiliCommand.php
+++ src/Command/MeiliCommand.php
@@ -38,7 +38,7 @@ class MeiliCommand extends Command
         $this->addArgument('latitude', InputArgument::OPTIONAL);
         $this->addArgument('longitude', InputArgument::OPTIONAL);
         $this->addArgument('distance', InputArgument::OPTIONAL);
-        $this->addArgument('keyword', "keyword", InputArgument::OPTIONAL, 'Search with keyword');
+        $this->addArgument('keyword',  InputArgument::OPTIONAL, 'Search with keyword');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
