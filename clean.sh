38a2deaab9d017dc3001ab9ebf35bccd1b9c0046
jfsenechal <jfsenechal@gmail.com>
2026-03-14 11:32:44 +0100

    Add `config:clear` to clean.sh script

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+php artisan config:clear
 php artisan optimize:clear --silent
 php artisan filament:optimize-clear --silent
 rm -f storage/logs/*.log
 php artisan filament:optimize --silent
-
