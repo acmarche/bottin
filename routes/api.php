27a28c33349828b8fdc8f491f1acc1d722db6849
jfsenechal <jfsenechal@gmail.com>
2026-03-15 14:40:01 +0100

    Update route URL segment from `rubrique` to `category` in fiches endpoint

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('bottin')->group(function (): void {
     Route::get('commerces', [LegacyBottinController::class, 'commerces']);
     Route::get('fiches', [LegacyBottinController::class, 'fiches']);
-    Route::get('fiches/rubrique/{category}', [LegacyBottinController::class, 'fichesByCategory']);
+    Route::get('fiches/category/{category}', [LegacyBottinController::class, 'fichesByCategory']);
     Route::get('fiche/{shop}', [LegacyBottinController::class, 'ficheById']);
     Route::get('fichebyslugname/{slug}', [LegacyBottinController::class, 'ficheBySlug']);
 });
