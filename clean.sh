55022bcbe0c56373d7621b1ae45c1c2c0a5f9d18
jfsenechal <jfsenechal@gmail.com>
2026-04-08 08:57:29 +0200

    force rm

--- clean.sh
+++ clean.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+rm bootstrap/cache/*.php
+rm -r bootstrap/cache/filament/panels/
+rm storage/framework/views/*.php
 php artisan config:clear --silent
 php artisan optimize:clear --silent
 php artisan filament:optimize-clear --silent
 rm -f storage/logs/*.log
-php artisan filament:optimize --silent
