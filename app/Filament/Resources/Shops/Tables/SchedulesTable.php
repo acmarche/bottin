339aa2b273e8d4d1b776d10fd5634d1608168561
jfsenechal <jfsenechal@gmail.com>
2026-04-08 15:33:17 +0200

    Refactor `Schedules

--- app/Filament/Resources/Shops/Tables/SchedulesTable.php
+++ app/Filament/Resources/Shops/Tables/SchedulesTable.php
@@ -29,7 +29,7 @@ final class SchedulesTable
                 TextColumn::make('day')
                     ->label('Jour')
                     ->sortable()
-                    ->formatStateUsing(fn(?int $state): string => match ($state) {
+                    ->formatStateUsing(fn (?int $state): string => match ($state) {
                         1 => 'Lundi',
                         2 => 'Mardi',
                         3 => 'Mercredi',
@@ -48,18 +48,17 @@ final class SchedulesTable
                 TextColumn::make('morning_start')
                     ->label('Matin')
                     ->state(function (Schedule $record): string {
-                        return
-                            $record->morning_start.' - '.$record->morning_end;
+                        return mb_substr((string) $record->morning_start, 0, 5).' - '.mb_substr((string) $record->morning_end, 0, 5);
                     }),
                 TextColumn::make('noon_start')
                     ->label('Après-midi')
                     ->state(function (Schedule $record): string {
-                        return $record->noon_start.' - '.$record->noon_end;
+                        return mb_substr((string) $record->noon_start, 0, 5).' - '.mb_substr((string) $record->noon_end, 0, 5);
                     }),
             ])
             ->headerActions([
                 CreateAction::make()
-                    ->hidden(fn(): bool => $countSchedule >= 7),
+                    ->hidden(fn (): bool => $countSchedule >= 7),
             ])
             ->recordActions([
                 Action::make('copy')
@@ -68,7 +67,7 @@ final class SchedulesTable
                     ->schema([
                         Select::make('day')
                             ->label('Jour cible')
-                            ->options(fn(Schedule $record): array => array_diff_key(
+                            ->options(fn (Schedule $record): array => array_diff_key(
                                 ScheduleForm::DAY_OPTIONS,
                                 [$record->day => true],
                             ))
