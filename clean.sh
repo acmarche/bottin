64027855e16f220967f310dddfe95b009833608a
jfsenechal <jfsenechal@gmail.com>
2026-04-08 10:07:06 +0200

    up

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm bootstrap/cache/*.php
-rm -r bootstrap/cache/filament/panels/
+rm -fr bootstrap/cache/filament/panels/
 rm storage/framework/views/*.php
 php artisan config:clear --silent
 php artisan optimize:clear --silent
