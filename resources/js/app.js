93dbb040289d7b14712a2c9d266e394fd75aa9db
jfsenechal <jfsenechal@gmail.com>
2026-04-08 09:38:42 +0200

    Add `scrollToTab` function to facilitate smooth scrolling and tab selection

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+
+window.scrollToTab = function (tabName) {
+    const tabs = document.querySelectorAll('[role="tab"]');
+    for (const tab of tabs) {
+        if (tab.textContent.toLowerCase().includes(tabName)) {
+            tab.click();
+            setTimeout(() => {
+                tab.closest('[role="tablist"]')?.parentElement?.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start',
+                });
+            }, 100);
+            break;
+        }
+    }
+};
