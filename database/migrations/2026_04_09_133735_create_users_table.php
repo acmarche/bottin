0560cabfe1a3444c2f56667c278861411b0693c1
Senechal jf <jfsenechal@gmail.com>
2026-04-09 15:42:13 +0200

    make `name` column nullable in Users table migration

--- database/migrations/2026_04_09_133735_create_users_table.php
+++ database/migrations/2026_04_09_133735_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('username')->nullable()->unique();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
