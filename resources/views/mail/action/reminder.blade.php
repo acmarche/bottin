0faef90fb104767d56e85d93be99aa4e250236a4
jfsenechal <jfsenechal@gmail.com>
2026-05-27 09:53:42 +0200

    Convert reminder email to use Markdown template for improved readability and maintainability

--- resources/views/mail/action/reminder.blade.php
+++ resources/views/mail/action/reminder.blade.php
@@ -3,6 +3,14 @@
 
     The body of your message.
     {{$shop->company}}
+
+    <x-mail::panel>
+        mon panel
+    {{$content}}
+    </x-mail::panel>
+
+    {{$content}}
+
     <x-mail::button :url="{{$url}}">
         Gérer les données de ma fiche
     </x-mail::button>
