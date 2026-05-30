603480d46fb270f1938d4d08e40a392a60d0ab16
jfsenechal <jfsenechal@gmail.com>
2026-05-30 14:29:00 +0200

    add view and route cache clear commands to clean script

--- clean.sh
+++ clean.sh
@@ -2,6 +2,8 @@
 rm bootstrap/cache/*.php
 rm -fr bootstrap/cache/filament/panels/
 rm storage/framework/views/*.php
+php artisan view:clear
+php artisan route:clear
 php artisan config:clear --silent
 php artisan optimize:clear --silent
 php artisan filament:optimize-clear --silent
