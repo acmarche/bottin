0a9945b566a85bf45543df016cd899fca3cf8043
jfsenechal <jfsenechal@gmail.com>
2026-05-31 10:35:27 +0200

    remove unused bottin routes

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,7 @@ use App\Http\Middleware\VerifyApiToken;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(VerifyApiToken::class)->prefix('bottin')->group(function (): void {
-    Route::get('commerces', [LegacyBottinController::class, 'commerces']);
     Route::get('fiches', [LegacyBottinController::class, 'fiches']);
     Route::get('fiches/category/{category}', [LegacyBottinController::class, 'fichesByCategory']);
     Route::get('fiche/{shop}', [LegacyBottinController::class, 'ficheById']);
-    Route::get('fichebyslugname/{slug}', [LegacyBottinController::class, 'ficheBySlug']);
 });
